e7de185377631fa176343f51c4afdf6bba714f67
Lucas Silva Meira <lucasmeiraprofessional@gmail.com>
2024-01-28 23:14:36 -0300

    Passo 3

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,18 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        double num1 = 20;
+        int num2 = 10;
+        char letra = 'a';
+        String texto = " ponte do terror";
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
+        double media = ((num1 + (double) num2 )/2);
+        System.out.println(media);
+
+        String historia = """
+                 A história começa com uma pedra que caiu e quebrou
+                """+letra+texto;
+        System.out.println(historia);
     }
 }
